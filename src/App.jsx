b8c27ef2a18a728776caa42ec33f1b279338c02f
tommaso.dare <tommaso.dare96@gmail.com>
2025-04-16 11:53:48 +0200

    Done Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
+
 export default function App() {
 
   return (
 
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+    </>
 
   )
-
 
 }

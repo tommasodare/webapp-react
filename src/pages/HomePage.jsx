bad103ea9ad41f732959ac957c06f56761be41c4
tommaso.dare <tommaso.dare96@gmail.com>
2025-04-16 13:08:36 +0200

    Done Milestone 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,52 @@
+import { useState, useEffect } from 'react';
+
 export default function HomePage() {
 
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/v1/movies')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setMovies(data)
+            }
+            )
+            .catch(error => console.error('Error fetching movies:', error));
+    }, [])
+
     return (
+
 
         <div className="home-page">
             <h1>Home Page</h1>
             <p>Welcome to the Home Page</p>
+
+            <h2>Movies</h2>
+
+            <section className="movies">
+                <div className="container">
+                    <div className=" row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                        {movies.map(movie => (
+                            <div key={movie.id}>
+                                <div className="card">
+                                    <img src={`http://localhost:3000/img/${movie?.image}`} alt="" className='c<rd-img-top' />
+                                    <div className="card-body">
+                                        <h3>{movie.title}</h3>
+                                        <p>{movie.genre}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+
         </div>
 
     )

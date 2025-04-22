ac47650e3571ec0cd72a3bafc1dd80ded93326f7
tommaso.dare <tommaso.dare96@gmail.com>
2025-04-22 11:53:46 +0200

    Add loading state management to DefaultLayout and App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState } from "react"
 import DefaultLayout from "./layouts/DefaultLayout.jsx"
 import HomePage from "./pages/HomePage.jsx"
 import MoviePage from "./pages/MoviePage.jsx"
@@ -6,11 +7,13 @@ import GlobalContext from "./contexts/GlobalContext.jsx"
 
 export default function App() {
 
+  const [isLoading, setIsLoading] = useState(false) // se è true sta caricando la pagina
+
   return (
 
     <>
 
-      <GlobalContext.Provider>
+      <GlobalContext.Provider value={{ isLoading, setIsLoading }}>
 
         <BrowserRouter>
           <Routes>
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,8 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
+import { useContext } from "react";
+import GlobalContext from "../contexts/GlobalContext.jsx";
 
 export default function DefaultLayout() {
+
+    const { isLoading } = useContext(GlobalContext)
+
+    if (isLoading) {
+
+        return (
+
+            <div className="d-flex justify-content-center align-items-center vh-100">
+                <div className="spinner-border">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+
+        )
+
+    }
 
     return (
 

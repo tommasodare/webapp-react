9dc161cb6a3c82fa9c06c0814e957993a52873be
tommaso.dare <tommaso.dare96@gmail.com>
2025-04-22 11:45:05 +0200

    Add GlobalContext for state management and wrap App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DefaultLayout from "./layouts/DefaultLayout.jsx"
 import HomePage from "./pages/HomePage.jsx"
 import MoviePage from "./pages/MoviePage.jsx"
+import GlobalContext from "./contexts/GlobalContext.jsx"
 
 export default function App() {
 
@@ -9,14 +10,19 @@ export default function App() {
 
     <>
 
-      <BrowserRouter>
-        <Routes>
-          <Route Component={DefaultLayout}>
-            <Route path="/" Component={HomePage} />
-            <Route path="/:id" Component={MoviePage} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalContext.Provider>
+
+        <BrowserRouter>
+          <Routes>
+            <Route Component={DefaultLayout}>
+              <Route path="/" Component={HomePage} />
+              <Route path="/:id" Component={MoviePage} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+
+      </GlobalContext.Provider>
+
 
     </>
 
